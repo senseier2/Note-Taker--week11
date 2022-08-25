a8dbb8ca47f13af443357078d6cb28d1b2642723
Senfire <eric.mcghee@northwestern.edu>
2022-08-25 16:14:01 -0500

    Just noticed my previous commit wasn't authenticated. Add new server js code and first routes (html routes)

---
+++ routes/html/index.js
@@ -0,0 +1,16 @@
+//Importing path
+const path = require('path');
+//importing the router - https://www.geeksforgeeks.org/express-js-express-router-function/
+const router = require('express').router();
+
+//route to the notes html file
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+module.exports = router;
+
