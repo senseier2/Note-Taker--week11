8b4c28d0ea2d065a65066a07c2fc8ca77dd2c7ef
Senfire <eric.mcghee@northwestern.edu>
2022-08-25 22:58:38 -0500

    found the array issue- and corrected the issue with the router for api route notes (discontinued the use of passing into an object

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,7 +1,7 @@
 //Importing the router
 const router = require('express').Router();
 //Initializing the db call and adding to the notes variable.
-const {notes} = require('../../db/db.json'); //Found some information on sharing a db between node and express as an object here: https://itnext.io/how-to-share-a-single-database-connection-in-a-node-js-express-js-app-fcad4cbcb1e
+const notes = require('../../db/db'); //Found some information on sharing a db between node and express as an object here: https://itnext.io/how-to-share-a-single-database-connection-in-a-node-js-express-js-app-fcad4cbcb1e
 
 //Create new notes
 const {
